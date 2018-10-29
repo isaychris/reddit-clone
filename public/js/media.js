f00fb6b9498e1fd919b5edc2ccd7a8cdeaa3a29f
isaychris <adaptxsurvive@gmail.com>
2018-10-29 01:38:04 -0700

    fixed issue with post not being submitted in subreddit; youtube videos are now embed

---
+++ public/js/media.js
@@ -0,0 +1,13 @@
+$(".post-body").ready(function () {
+    let query = $('.post-body')
+    let url = query.text()
+
+    function youtube_parser(url){
+        var regExp = /^.*((youtu.be\/)|(v\/)|(\/u\/\w\/)|(embed\/)|(watch\?))\??v?=?([^#\&\?]*).*/;
+        var match = url.match(regExp);
+        return (match&&match[7].length==11)? match[7] : false;
+    }
+
+    let id = youtube_parser(url)
+    query.html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
+})
--- routes/subreddit.js
+++ routes/subreddit.js
@@ -9,10 +9,11 @@ let submit_controller = require("../controllers/submit_controller")
 // ROUTES
 router.get('/:subreddit', subreddit_controller.get_all);
 router.get('/:subreddit/:id/comments', subreddit_controller.get_post);
-router.post('/:subreddit/:id/comments', comment_controller.comment);
 router.get('/:subreddit/submit/post', submit_controller.subreddit_post_view);
-router.post('/:subreddit/submit/post', submit_controller.subreddit_post_view);
 router.get('/:subreddit/submit/link', submit_controller.subreddit_link_view);
+
+router.post('/:subreddit/submit/post', submit_controller.subreddit_post);
+router.post('/:subreddit/:id/comments', comment_controller.comment);
 router.post('/:subreddit/submit/link', submit_controller.subreddit_link);
 router.post('/:subreddit/search', submit_controller.subreddit_search);
 
