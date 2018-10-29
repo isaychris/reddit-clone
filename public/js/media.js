432b7ffedd3381646532253f6a66dd4a7d0b8856
isaychris <adaptxsurvive@gmail.com>
2018-10-29 01:41:27 -0700

    quick fix with all posts being replaced with youtube embed; test your code!

--- public/js/media.js
+++ public/js/media.js
@@ -9,5 +9,8 @@ $(".post-body").ready(function () {
     }
 
     let id = youtube_parser(url)
-    query.html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
+    
+    if(id) {
+        query.html(`<iframe width="560" height="315" src="https://www.youtube.com/embed/${id}" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>`);
+    }
 })
