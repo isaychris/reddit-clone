9bf61684b478ab7a333e1b048a85502698673623
Yew Kang Wei <yewyew6933@gmail.com>
2020-10-27 11:37:48 +0800

    remove MongoDb warnings

--- configs/database.js
+++ configs/database.js
@@ -1,7 +1,10 @@
 module.exports.config = {
-    uri: 'mongodb+srv://admin:anime@cluster0-e7lsm.mongodb.net/test?retryWrites=true',
-    options: {
-        dbName: 'reddit-app',
-        useNewUrlParser: true
-    },
-}
+  uri:
+    "mongodb+srv://admin:anime@cluster0-e7lsm.mongodb.net/test?retryWrites=true",
+  options: {
+    dbName: "reddit-app",
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+  },
+};
